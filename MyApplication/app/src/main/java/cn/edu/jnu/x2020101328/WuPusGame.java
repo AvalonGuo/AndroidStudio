a1063c05321dcb505dd04726ad091cf82764a115
塔其米 <1339296390@qq.com>
2023-03-28 12:02:55 +0800

    test1:setPlayerRoom

---
+++ MyApplication/app/src/main/java/cn/edu/jnu/x2020101328/WuPusGame.java
@@ -0,0 +1,7 @@
+package cn.edu.jnu.x2020101328;
+
+public class WuPusGame {
+    public int setPlayerRoom(int i) {
+        return i;
+    }
+}
---
+++ MyApplication/app/src/test/java/cn/edu/jnu/x2020101328/HuntGameUnitTest.java
@@ -0,0 +1,19 @@
+package cn.edu.jnu.x2020101328;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class HuntGameUnitTest {
+    @Test
+    public void testMove(){
+        WuPusGame game = new WuPusGame();
+        game.setPlayerRoom(4);
+        assertEquals(4,game.setPlayerRoom(4));
+    }
+}
